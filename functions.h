3b9778ea1f969bf9b93368e7bfde6c4619862e14
Julia Zbińska <julia.zbinska@gmail.com>
2025-01-08 13:51:39 +0100

    dodalam deklaracje funkcji

--- functions.h
+++ functions.h
@@ -5,14 +5,17 @@
 /*interfejs graficzny*/ 
 void UpdateHitbox(struct ship* s);	//ustala pozycję i wymiary hitbox'u
 void mouse_drag(int key, struct ship* s, Color color);//ustala położenie obiektu po pozycji mysz}
-void rotate(int key, Image* sprite, Texture2D* texture);	//ustala rotację obiektu
+void rotate(int key, Image* sprite, Texture2D* texture);   //ustala rotację obiektu
 void UpdateShip(bool* isDragging, struct ship* s);	//wywołuje mouse_drag i rotate
-/*plansza i interakcja z plansza*/ 
+/*plansza i interakcja z plansza*/
 board* initboard();
 void delboard(board* boardtab);
 void placeStatek(board* board,ship* curr_ship,pair begin,int direction);
 void shoot(board* player,pair pairA);//wymaga poprawnie zainicjowanego statku zeby dzialalo, a wiec potrzeba inicjatora
 void printboard(board* boardtab);//funkcja rysuje tablice danego gracza. Na razie drukuje tylko tile'a (funkcja potencjalnie do wymiany)
+struct array_cordinals* Get_array_cordinals(int offsetX, int offsetY);//przekształca położenie myszki na położenie statku na planszy
+/*gameplay*/
+void PlayGame(board *playerBoard, board *enemyBoard, ship *playerShip, ship *enemyShip); //funkcja do obslugi gry (inicjuje okienko oraz dodany jest jakis biedny postgame)
 /*do dodania*/
 ship* initship(int type);
 void delship(ship* statek);
