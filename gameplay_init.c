0f8180a147cdbc32fe97f5770ad3cb50b4295a76
Bartłomiej Powierża <mozartwa123@gmail.com>
2025-01-11 17:31:21 +0100

    Memoire (#13)
    * dodałem sprawdzanie pamięci w niktórych funkcjach i w gameplay_init.c Niestety dalej jest problem z double free, z pomocą niektórych narzędzi i czata GPT wyszło, że CloseWindow() i UnloadTexture() zwalniają tę samą pamięć, ale nie ręczę, że tak jest. Był problem w funkcji PlayGame - nietrafianie w planszę skutkowało zamknięciem programu, co jest niepożądane, zastosowałem ordynarne rozwiązanie - goto VALIDCLICK, można zastąpić to instrukcją warunkową - ale oznaczało by to dodatkową pracę i możliwe błędy. Dodatkowo funkcja get_array_cordinal (zresztą moja, ale chyba to nie ja wprowadziłem ten błąd- ale pewny nie jestem) niedopuszczała współrzędnych z 9, co nie było poprawne. To też poprawiłem.
    
    * dodałem sprawdzanie pamięci w niktórych funkcjach i w gameplay_init.c Niestety dalej jest problem z double free, z pomocą niektórych narzędzi i czata GPT wyszło, że CloseWindow() i UnloadTexture() zwalniają tę samą pamięć, ale nie ręczę, że tak jest. Był problem w funkcji PlayGame - nietrafianie w planszę skutkowało zamknięciem programu, co jest niepożądane, zastosowałem ordynarne rozwiązanie - goto VALIDCLICK, można zastąpić to instrukcją warunkową - ale oznaczało by to dodatkową pracę i możliwe błędy. Dodatkowo funkcja get_array_cordinal (zresztą moja, ale chyba to nie ja wprowadziłem ten błąd- ale pewny nie jestem) niedopuszczała współrzędnych z 9, co nie było poprawne. To też poprawiłem.
    
    * dodałem sprawdzanie pamięci w niktórych funkcjach i w gameplay_init.c Niestety dalej jest problem z double free, z pomocą niektórych narzędzi i czata GPT wyszło, że CloseWindow() i UnloadTexture() zwalniają tę samą pamięć, ale nie ręczę, że tak jest. Był problem w funkcji PlayGame - nietrafianie w planszę skutkowało zamknięciem programu, co jest niepożądane, zastosowałem ordynarne rozwiązanie - goto VALIDCLICK, można zastąpić to instrukcją warunkową - ale oznaczało by to dodatkową pracę i możliwe błędy. Dodatkowo funkcja get_array_cordinal (zresztą moja, ale chyba to nie ja wprowadziłem ten błąd- ale pewny nie jestem) niedopuszczała współrzędnych z 9, co nie było poprawne. To też poprawiłem.

--- gameplay_init.c
+++ gameplay_init.c
@@ -1,15 +1,34 @@
 #include "raylib.h"
 #include "structs.h"
 #include "functions.h"
+#include <stdlib.h>
 /*main do testowania gameplayu*/
 int main()
 {
     board *playerBoard = initboard();
+    if(playerBoard == NULL){//przyda sie sprawdzic, czy alokacja sie powiodla
+        return 1;
+    }
     board *enemyBoard = initboard();
+    if(enemyBoard == NULL){//przyda sie sprawdzic, czy alokacja sie powiodla
+        delboard(playerBoard);
+        return 1;
+    }
 
     //reczne dodanie statkow do debugowania (w przyszlosci zmienione na to co dostaniemy od gracza)
     ship *playerShip = initship(3);
+    if(playerShip == NULL){
+        delboard(playerBoard);
+        delboard(enemyBoard);
+        return 1;
+    }
     ship *enemyShip = initship(3);
+    if(playerShip == NULL){
+        delboard(playerBoard);
+        delboard(enemyBoard);
+        delship(playerShip);
+        return 1;
+    }
     pair playerStart = {2, 2};
     pair enemyStart = {4, 4};
     placeStatek(playerBoard, playerShip, playerStart, 2);
@@ -19,6 +38,7 @@ int main()
     PlayGame(playerBoard, enemyBoard,playerShip,enemyShip);
 
     //sprzatam i wychodze
+
     delboard(playerBoard);
     delboard(enemyBoard);
     delship(playerShip);
